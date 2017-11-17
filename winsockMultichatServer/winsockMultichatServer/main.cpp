0b7e2194c142372a6e5968300f97742be60ba8ea
Unknown <trephneor@gmail.com>
2017-11-17 09:51:28 +0100

    Multiple clients implemented! WHOO!

--- winsockMultichatServer/winsockMultichatServer/main.cpp
+++ winsockMultichatServer/winsockMultichatServer/main.cpp
@@ -17,8 +17,61 @@ struct clientStruct
 {
 	int id;
 	SOCKET socket;
-	std::string nickname;
 };
+
+const int MAX_CLIENTS = 5;
+
+
+int handleClient(clientStruct &newClient, std::vector<clientStruct> &clientsArray, std::thread &thread)
+{
+	std::string messageToSend = "";
+	char receivedMessage[BUFLEN] = "";
+//Session
+while (1)
+{
+	memset(receivedMessage, 0, BUFLEN);
+
+	if (newClient.socket != 0)
+	{
+		int iResult = recv(newClient.socket, receivedMessage, BUFLEN, 0);
+
+		if (iResult == SOCKET_ERROR || !strcmp(receivedMessage, "exit"))
+		{
+			messageToSend = "Disconnected!";
+
+			std::cout << messageToSend << std::endl;
+
+			closesocket(newClient.socket);
+			closesocket(clientsArray[newClient.id].socket);
+			clientsArray[newClient.id].socket = INVALID_SOCKET;
+
+			//Broadcast the disconnection message to the other clients
+			for (int i = 0; i < MAX_CLIENTS; i++)
+			{
+				if (clientsArray[i].socket != INVALID_SOCKET)
+					iResult = send(clientsArray[i].socket, messageToSend.c_str(), strlen(messageToSend.c_str()), 0);
+			}
+
+			break;
+		}
+		else if (strcmp("", receivedMessage))
+			messageToSend = receivedMessage;
+		std::cout << messageToSend.c_str() << std::endl;
+
+		//Broadcast the sent message to the other clients
+		for (int i = 0; i < MAX_CLIENTS; i++)
+		{
+			if (clientsArray[i].socket != INVALID_SOCKET)
+				if (newClient.id != i)
+					iResult = send(clientsArray[i].socket, messageToSend.c_str(), strlen(messageToSend.c_str()), 0);
+		}
+	}
+} //end while
+
+thread.detach();
+
+return 0;
+}
 
 int main() {
 
@@ -27,6 +80,11 @@ int main() {
 	struct addrinfo *server = NULL;
 	SOCKET serverSocket = INVALID_SOCKET;
 	std::string initMessage = "";
+	std::vector<clientStruct> clients(MAX_CLIENTS);
+
+	int tempId = -1;
+
+	std::thread clientThread[MAX_CLIENTS];
 
 	//Initialize Winsock
 
@@ -55,6 +113,12 @@ int main() {
 	std::cout << "Awaiting for the client to connect" << std::endl;
 	listen(serverSocket, SOMAXCONN);
 
+	//Initialize list of clients
+	for (int i = 0; i < MAX_CLIENTS; i++)
+	{
+		clients[i] = { -1, INVALID_SOCKET };
+	}
+
 
 	while (1)
 	{
@@ -64,10 +128,50 @@ int main() {
 
 		if (incomingClient == INVALID_SOCKET)
 			continue;
-	}
+
+
+
+		//Create a temporary id for the next client
+		tempId = -1;
+		for (int i = 0; i < MAX_CLIENTS; i++)
+		{
+			if (clients[i].socket == INVALID_SOCKET && tempId == -1)
+			{
+				clients[i].socket = incomingClient;
+				clients[i].id = i;
+				tempId = i;
+			}
+
+		}
+
+		if (tempId != -1)
+		{
+			//Send the id to that client
+			initMessage = std::to_string(clients[tempId].id);
+			send(clients[tempId].socket, initMessage.c_str(), strlen(initMessage.c_str()), 0);
+
+			//Create a thread process for that client
+			clientThread[tempId] = std::thread(handleClient, std::ref(clients[tempId]), std::ref(clients), std::ref(clientThread[tempId]));
+		}
+		else
+		{
+			initMessage = "Server is full!";
+			send(incomingClient, initMessage.c_str(), strlen(initMessage.c_str()), 0);
+			std::cout << initMessage << std::endl;
+		}
+	} //end while
+
+
 
 	//Close listening socket
 	closesocket(serverSocket);
+
+	//Close client socket
+	for (int i = 0; i < MAX_CLIENTS; i++)
+	{
+		clientThread[i].detach();
+		closesocket(clients[i].socket);
+	}
 
 	//Clean up Winsock
 	WSACleanup();
